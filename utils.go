89ed0576dba3f8e48b27dd2a570a6f7a27948dcb
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-10-03 17:25:42 -0700

    Add Show conditional

--- utils.go
+++ utils.go
@@ -1,5 +1,14 @@
 package elem
 
+// Show conditionally renders one of the provided elements based on the condition
+func Show(condition bool, ifTrue, ifFalse *Element) *Element {
+	if condition {
+		return ifTrue
+	}
+	return ifFalse
+}
+
+// TransformEach maps a slice of items to a slice of Elements using the provided function
 func TransformEach[T any](items []T, fn func(T) *Element) []*Element {
 	var elements []*Element
 	for _, item := range items {
--- utils_test.go
+++ utils_test.go
@@ -5,6 +5,17 @@ import (
 	"testing"
 )
 
+func TestShow(t *testing.T) {
+	trueElement := Div(nil, Text("True Condition"))
+	falseElement := Div(nil, Text("False Condition"))
+
+	resultTrue := Show(true, trueElement, falseElement)
+	assert.Equal(t, trueElement.Render(), resultTrue.Render())
+
+	resultFalse := Show(false, trueElement, falseElement)
+	assert.Equal(t, falseElement.Render(), resultFalse.Render())
+}
+
 func TestTransformEach(t *testing.T) {
 	items := []string{"Item 1", "Item 2", "Item 3"}
 
