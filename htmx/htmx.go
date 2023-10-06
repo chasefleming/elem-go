bb6c42be97da896137a6074ef5c4140372947175
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-10-05 20:01:36 -0700

    Add htmx attrs

---
+++ htmx/htmx.go
@@ -0,0 +1,33 @@
+package htmx
+
+const (
+	// Request Modifiers
+	HXGet    = "hx-get"
+	HXPost   = "hx-post"
+	HXPut    = "hx-put"
+	HXDelete = "hx-delete"
+	HXPatch  = "hx-patch"
+
+	// Request Parameters
+	HXParams = "hx-params"
+	HXValues = "hx-values"
+
+	// Response Processing
+	HXSwap    = "hx-swap"
+	HXTarget  = "hx-target"
+	HXSwapOOB = "hx-swap-oob"
+
+	// Events
+	HXTrigger = "hx-trigger"
+	HXConfirm = "hx-confirm"
+
+	// Indicators
+	HXIndicator = "hx-indicator"
+
+	// History
+	HXPushURL = "hx-push-url"
+
+	// Error Handling
+	HXBoost = "hx-boost"
+	HXError = "hx-error"
+)
