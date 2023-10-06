af416ddd4db16469ed00da8b679d3b2f7f861e23
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-10-05 20:04:55 -0700

    Add basic props

---
+++ props.go
@@ -0,0 +1,17 @@
+package elem
+
+type Props map[string]string
+
+const (
+	ID      = "id"
+	Charset = "charset"
+	Class   = "class"
+	Href    = "href"
+	Lang    = "lang"
+	Src     = "src"
+	Alt     = "alt"
+	Width   = "width"
+	Height  = "height"
+	Target  = "target"
+	Rel     = "rel"
+)
