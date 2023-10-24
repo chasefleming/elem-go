a32bcfedcb867350f08d97d1d6eb83bcd2af73c5
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-10-24 08:41:53 -0700

    Add additional htmx attrs

--- htmx/htmx.go
+++ htmx/htmx.go
@@ -8,26 +8,46 @@ const (
 	HXDelete = "hx-delete"
 	HXPatch  = "hx-patch"
 
+	// Request Headers and Content-Type
+	HXHeaders = "hx-headers"
+	HXContent = "hx-content"
+
 	// Request Parameters
 	HXParams = "hx-params"
 	HXValues = "hx-values"
+
+	// Request Timeout and Retries
+	HXTimeout      = "hx-timeout"
+	HXRetry        = "hx-retry"
+	HXRetryTimeout = "hx-retry-timeout"
 
 	// Response Processing
 	HXSwap    = "hx-swap"
 	HXTarget  = "hx-target"
 	HXSwapOOB = "hx-swap-oob"
+	HXSelect  = "hx-select"
+	HXExt     = "hx-ext"
+	HXVals    = "hx-vals"
 
 	// Events
-	HXTrigger = "hx-trigger"
-	HXConfirm = "hx-confirm"
+	HXTrigger           = "hx-trigger"
+	HXConfirm           = "hx-confirm"
+	HXOn                = "hx-on"
+	HXTriggeringElement = "hx-triggering-element"
+	HXTriggeringEvent   = "hx-triggering-event"
 
 	// Indicators
 	HXIndicator = "hx-indicator"
 
 	// History
-	HXPushURL = "hx-push-url"
+	HXPushURL     = "hx-push-url"
+	HXHistoryElt  = "hx-history-elt"
+	HXHistoryAttr = "hx-history-attr"
 
 	// Error Handling
 	HXBoost = "hx-boost"
 	HXError = "hx-error"
+
+	// Caching
+	HXCache = "hx-cache"
 )
