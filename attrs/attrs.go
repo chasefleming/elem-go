c4c70bc763ae62a9f1a9dc819d8b143ca7f02fa1
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-11-14 19:46:47 -0800

    Add size to attrs list

--- attrs/attrs.go
+++ attrs/attrs.go
@@ -63,6 +63,7 @@ const (
 	Required       = "required"
 	Rows           = "rows"
 	Selected       = "selected"
+	Size           = "size"
 	Step           = "step"
 	Type           = "type"
 	Value          = "value"
