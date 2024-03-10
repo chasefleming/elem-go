65f74ad8b85f5c368c7f693326122c9f4e4a9ff4
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2024-03-10 09:25:54 -0700

    Create `Merge` method for attributes

--- attrs/utils.go
+++ attrs/utils.go
@@ -2,10 +2,21 @@ package attrs
 
 import "strings"
 
-// DataAttr returns the name for a data attribute
+// DataAttr returns the name for a data attribute.
 func DataAttr(name string) string {
 	var builder strings.Builder
 	builder.WriteString(DataPrefix)
 	builder.WriteString(name)
 	return builder.String()
+}
+
+// Merge merges multiple attribute maps into a single map, with later maps overriding earlier ones.
+func Merge(attrMaps ...Props) Props {
+	mergedAttrs := Props{}
+	for _, attrMap := range attrMaps {
+		for key, value := range attrMap {
+			mergedAttrs[key] = value
+		}
+	}
+	return mergedAttrs
 }
