f35d77934de811003dc7314ead3d6fb18d777894
Alex Svetkin <alex.svetkin@gmail.com>
2025-07-15 10:53:40 +0200

    string replace optimizations

--- utils.go
+++ utils.go
@@ -2,6 +2,15 @@ package elem
 
 import "strings"
 
+// nodeContentReplacer handles escaping of HTML special characters in text nodes
+// note that single and double quotes (', ") do not need escaping in HTML5 text nodes
+var nodeContentReplacer = strings.NewReplacer(
+	"&", "&amp;",
+	"<", "&lt;",
+	// technically, > does not need escaping in HTML5, but we do it for consistency
+	">", "&gt;",
+)
+
 // If conditionally renders one of the provided elements based on the condition
 func If[T any](condition bool, ifTrue, ifFalse T) T {
 	if condition {
@@ -21,11 +30,5 @@ func TransformEach[T any](items []T, fn func(T) Node) []Node {
 
 // EscapeNodeContents escapes HTML5 special characters in a string to ensure safe rendering as a text node
 func EscapeNodeContents(s string) string {
-	s = strings.ReplaceAll(s, "&", "&amp;")
-	s = strings.ReplaceAll(s, "<", "&lt;")
-	// technically, > does not need escaping in HTML5, but we do it for consistency
-	s = strings.ReplaceAll(s, ">", "&gt;")
-	// note that single and double quotes (', ") do not need escaping in HTML5 text nodes
-
-	return s
+	return nodeContentReplacer.Replace(s)
 }
